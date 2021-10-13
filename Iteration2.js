ee9e41714663956d21636ebe71a9e69c59576cad
unknown <javimorenomontejo@gmail.com>
2021-10-13 17:33:29 +0200

    iteration#2

---
+++ Iteration2.js
@@ -0,0 +1,30 @@
+//2.1
+const game = {title: 'The last us 2', gender: ['action', 'zombie', 'survival'], year: 2020};
+
+const {title, gender, year} = game;
+console.log(title,gender,year)
+
+//2.2
+const fruits = ['Banana', 'Strawberry', 'Orange'];
+const [x, y, z ] = fruits;
+console.log(x);
+console.log(y);
+console.log(z);
+
+//2.3
+const animalFunction = () => {
+    return {name: 'Bengal Tiger', race: 'Tiger'}
+};
+
+let {name, race} = animalFunction()
+console.log(name)
+
+//2.4
+const car = {name: 'Mazda 6', itv: [2015, 2011, 2020] };
+
+let {name, itv} = car;
+let [year1, year2, year3] = itv;
+
+console.log(year1);
+console.log(year2);
+console.log(year3);
